fdbeeb99b645b00c822a5b2d5b2f60095db712b8
nihalnaveed12 <nihalnaveed50@gmail.com>
2025-05-06 03:06:48 +0500

    practice practice practice

---
+++ practice.py
@@ -0,0 +1,43 @@
+# print("Hello")
+
+# myList = ["Apple", "Mango", "Banana", "Strawberry"]
+
+# for fruit in myList: 
+#     print(fruit , end="-")
+
+# age centralization 
+
+# user_age = input("Enter your age: ")
+
+# age = int(user_age)
+
+# if age < 13: 
+#     print("You are a child")  
+# elif age < 20: 
+#     print("You are a teen ager")
+# elif age < 30: 
+#     print("You are a young adult")
+# elif age < 60: 
+#     print("you are a adult")
+# else:
+#     print("You are a senior")    
+
+# movie ticket price and discount calculation 
+
+# user_age = input("Enter your age: ")
+# age = int(user_age)
+# price = 20 if age > 20 else 8 
+# options = ["wed", "thurs", "fri", "sat", "sun"]
+# print(options)
+# print("If you select wed you will get 20% discount")
+# day = input("Enter your day: ")
+
+# if day == "wed":
+#     print("You get a 2$ discount")
+#     print("Your ticket price is", price)
+# else:
+#   print("Your ticket price is: ", price)
+
+
+
+    
